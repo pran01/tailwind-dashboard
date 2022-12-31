490808cada2fbdb32deacdd5121752b1062b9146
Pranav Sinha <pran.sinha1.0@gmail.com>
2022-12-31 14:30:14 +0530

    completed header and made responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      fgcolor: "#2c3034",
+      "fgcolor-active": "#34383c",
+      fontclr: "#7f7f7f",
+      "fontclr-active": "#bbbbbd",
+      "iconclr-active": "#f66673",
+      bgcolor: "#23242b",
+      borderclr: "#5d4192",
+    },
   },
   plugins: [],
 };
