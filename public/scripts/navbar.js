c135b737d62d94674a195cfb64623f3012f09a08
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-01-04 00:44:13 +0530

    added animation to navbar

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -5,15 +5,31 @@ let navList = document.querySelector(".nav-list");
 let helpList = document.querySelector(".help-list");
 let header = document.querySelector("header");
 let toggleIcon = document.querySelector(".toggle-icon");
+let navBarCloseBtn = document.querySelector(".navbar-close-btn");
+
+var id = null;
+function moveNavBar() {
+  var width = 0;
+  clearInterval(id);
+  id = setInterval(frame, 1);
+  function frame() {
+    if (width == 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      navBar.style.width = width + "vw";
+      navBar.style.opacity = width + "%";
+    }
+  }
+}
+
 navBarBtn.addEventListener("click", () => {
+  moveNavBar();
+  navBar.classList.toggle("hidden");
   navBarTitle.classList.toggle("hidden");
   navList.classList.toggle("hidden");
   helpList.classList.toggle("hidden");
-  navBar.classList.toggle("w-[22px]");
-  navBar.classList.toggle("w-screen");
   header.classList.toggle("hidden");
-  toggleIcon.classList.toggle("fa-chevron-right");
-  toggleIcon.classList.toggle("fa-chevron-left");
   navBar.classList.toggle("flex");
   navBar.classList.toggle("flex-col");
   helpList.classList.toggle("flex");
@@ -22,4 +38,14 @@ navBarBtn.addEventListener("click", () => {
   navList.classList.toggle("flex");
   navList.classList.toggle("flex-col");
   navList.classList.toggle("items-center");
+  navBarCloseBtn.style.right = "1rem";
+  navBarCloseBtn.style.top = "50%";
+});
+
+navBarCloseBtn.addEventListener("click", () => {
+  navBar.classList.toggle("hidden");
+  navBarTitle.classList.toggle("hidden");
+  header.classList.toggle("hidden");
+  navList.classList.toggle("hidden");
+  helpList.classList.toggle("hidden");
 });
